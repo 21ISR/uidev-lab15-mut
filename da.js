71da52adccee74ae93d8aa2094fdf5d9937adc7c
legenda <nelliprofi@gmail.com>
2025-05-24 03:33:19 +0000

    {finish iou}

--- da.js
+++ da.js
@@ -1,28 +1,37 @@
-const display = document.querySelectorAll('.display');
+const display = document.querySelector('.display');
 
 let batons = document.querySelectorAll('.button');
 
-
-// batons.map((button) => {
-//     button.addEventListener("click", () => {
-//         if (button === 'AC') {
-
-//         }
-
-
-//     });
-// });
-
 function handleButton(event) {
-    console.log(event.target.textContent);
+    console.log(display);
 
-    if (event === 'AC') {
+    if (event.target.textContent === 'AC') {
         display.textContent = 0
-    } else if (event === '+') {
-        display.textContent = eval(number1 + number2)
-    } else if (event === '-') {
-         display.textContent = eval(number1 - number2)
+    } else if (event.target.textContent === '×') {
+        display.textContent += "×"
+    } else if (event.target.textContent === '÷') {
+        display.textContent += "÷"
+    } else if (event.target.textContent === '+/-') {
+        display.textContent = eval(display.textContent)
+        if (display.textContent < 0) {
+            display.textContent = display.textContent.slice(1)
+        } else {
+            display.textContent = "-" + display.textContent
+        }
+    } else if (event.target.textContent === '%') {
+        display.textContent = eval(display.textContent) + "%"
+    } else if (event.target.textContent === '=') {
+        const result = eval(display.textContent.replaceAll("×", "*").replaceAll("÷", "/"))
+        console.log(result);
+        display.textContent = result
+    } else {
+        if (display.textContent === "0" && event.target.textContent !== ".") {
+            display.textContent = event.target.textContent
+        } else {
+            display.textContent += event.target.textContent
+        }
     }
+
 }
 
 batons.forEach(baton => {
